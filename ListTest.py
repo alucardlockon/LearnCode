6ee61d8bea6b854ba71d7321252e36d11fff3234
Yu Wenhui <alucardlockon3@gmail.com>
2016-07-05 22:10:22 +0800

    PythonListTest
    20160705-PythonListTest

---
+++ ListTest.py
@@ -0,0 +1,9 @@
+#test List
+print "Please insert your List,type 'exit' to exit";
+ls=[];
+value="";
+while value!="exit":
+    value=raw_input();
+    ls.append(value);
+print "your List is:"+str(ls);
+raw_input();
