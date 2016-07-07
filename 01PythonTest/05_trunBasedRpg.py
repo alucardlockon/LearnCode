d8b2a93c12dcca7e6e71caa28804e2d040198657
alucardlockon <alucardlockon3@gmail.com>
2016-07-07 12:36:55 +0800

    005 version2

--- 01PythonTest/05_trunBasedRpg.py
+++ 01PythonTest/05_trunBasedRpg.py
@@ -11,6 +11,8 @@ class role:
     defe=5
     rest=5
     void=5
+    dropItems=[None]
+    dropPrecent=[100]
     command=['attack','void','def','fireball']
     def __init__(self,name,lv):
         self.name=name
@@ -18,16 +20,52 @@ class role:
         self.initRoleByLv(lv)
     def initRoleByLv(self,lv):
         self.exp=lv*(1000+lv*200)
-        self.nextLv=(lv+1)*(1000+(lv+1)*100)-self.exp
-        stra=lv*1.1*random.random()
-    def showInfo(self):
+        self.nextLv=(lv+1)*(1000+(lv+1)*200)
+        self.stra=int(self.stra+lv*2*random.random())
+        self.inte=int(self.inte+lv*2*random.random())
+        self.spd=int(self.spd+lv*2*random.random())
+        self.defe=int(self.defe+lv*2*random.random())
+        self.rest=int(self.rest+lv*2*random.random())
+        self.void=int(self.void+lv*2*random.random())
+    def getInfo(self):
         return self.name+"[lv:"+str(self.lv)+",exp:"+str(self.exp)+\
                ",nextLv:"+str(self.nextLv)+\
                ",stra:"+str(self.stra)+",inte:"+str(self.inte)+\
                ",spd:"+str(self.spd)+",defe:"+str(self.defe)+\
                ",rest:"+str(self.rest)+\
                ",void:"+str(self.void)+",command:["+",".join(self.command)+"]]"
-        
+    def addExp(self,exp):
+        self.exp+=exp
+        if self.exp>=self.nextLv:
+            self.lvUp();
+    def lvUp(self):
+        self.lv+=1
+        self.nextLv=(self.lv+1)*(1000+(self.lv+1)*200)
+        self.stra=int(self.stra+2*random.random())
+        self.inte=int(self.inte+2*random.random())
+        self.spd=int(self.spd+2*random.random())
+        self.defe=int(self.defe+2*random.random())
+        self.rest=int(self.rest+2*random.random())
+        self.void=int(self.void+2*random.random())
+        if self.exp>=self.nextLv:
+            self.lvUp();
 
-my=role('my',2)
-print my.showInfo()
+class stage:
+    stagename="stage"
+    stageLv=1
+    compelete=False
+    startPos=0
+    endPos=100
+    emenyLIst=[role("man",1)]
+    emenyPrecent=[100]
+    boss=role("boss",3)
+    def __init__(self,stagename,stagelv):
+        self.stagename=stagename
+        self.stagelv=stagelv
+        self.startPos=0
+
+#my=role('my',7)
+#print my.getInfo()
+#my.addExp(18000)
+#print my.getInfo()
+stage=stage("forest",1)
