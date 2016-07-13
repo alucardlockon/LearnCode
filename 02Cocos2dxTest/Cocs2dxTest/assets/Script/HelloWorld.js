3bdebb2d9fbb71e4afd80a3a0a21270fbfeed451
Yu Wenhui <alucardlockon3@gmail.com>
2016-07-13 19:49:39 +0800

    0713 test commit

--- 02Cocos2dxTest/Cocs2dxTest/assets/Script/HelloWorld.js
+++ 02Cocos2dxTest/Cocs2dxTest/assets/Script/HelloWorld.js
@@ -1,6 +1,6 @@
 cc.Class({
     extends: cc.Component,
-
+    
     properties: {
         label: {
             default: null,
@@ -12,10 +12,26 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
         this.label.string = this.text;
+        this.label.string = "every day young life junes!";
+        / 添加键盘事件监听器
+        var listener = {
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function (keyCode, event) {
+                cc.log('keyDown: ' + keyCode);
+            },
+            onKeyReleased: function (keyCode, event) {
+                cc.log('keyUp: ' + keyCode);
+            }
+        }
+        // 绑定键盘事件
+        cc.eventManager.addListener(listener, this.node);
     },
-
+    
     // called every frame
     update: function (dt) {
+        
+        
 
+        
     },
 });
