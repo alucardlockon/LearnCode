cebbf6c0b7959d5f6fe70719da6d4e335d81c1af
Yu Wenhui <alucardlockon3@gmail.com>
2016-09-20 20:15:09 +0800

    commit

--- MyWeibo/routes/app.js
+++ MyWeibo/routes/app.js
@@ -5,8 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('app', { title: '辉大微博' });
+router.get('/myWeiBo', function(req, res, next) {
+    res.render('myWeiBo', { title: '辉大微博' });
 });
+
+router.get('/myFollow', function(req, res, next) {
+    res.render('myFollow', { title: '辉大微博' });
+});
+
+router.get('/logout', function(req, res, next) {
+
+});
+
 
 module.exports = router;
